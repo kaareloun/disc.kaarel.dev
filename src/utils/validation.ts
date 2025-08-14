5a6bd3679b67918eba98ace4e087d908f06518d1
Kaarel <kaareloun@gmail.com>
2025-08-14 17:33:11 +0300

    fix: allow no name

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const validateDGMetrixCompetitionsData = z.array(
   z.tuple([
-    z.string().min(1),
-    z.string().min(1),
-    z.number(),
-    z.number(),
-    z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/),
-    z.string().regex(/^\d{2}:\d{2}$/),
-    z.string().min(1),
-    z.string().min(1),
-    z.string(),
+    z.string().min(1), // id
+    z.string(), // name
+    z.number(), // lat
+    z.number(), // lon
+    z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/), // date
+    z.string().regex(/^\d{2}:\d{2}$/), // time
+    z.string().min(1), // competitionType
+    z.string().min(1), // courseAndLayout
+    z.string(), // description
   ]),
 );
